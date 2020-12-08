6c0f847d3345756c78623b9c82ffaf05ece0919e
Seain Malkin <seainmalkin@gmail.com>
2020-12-08 14:25:15 +0800

    First activity_lexicon_list.xml

---
+++ app/src/main/java/com/example/lexicon_memoria/LexiconListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lexicon_memoria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LexiconListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lexicon_list)
+    }
+}
